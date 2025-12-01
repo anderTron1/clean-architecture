463682b25aa1985a5cb13b54ef8077e29166aa29
anderTron1 <andre-luizpiresguimaraes@outlook.com>
2025-12-01 18:33:50 -0300

    testing

---
+++ example.py
@@ -0,0 +1,9 @@
+def minhaFuncao():
+    print("Olá, Mundo!")
+
+def essa_e_minha_funcao():
+    print("Essa é minha função!")
+
+# essa_e_minha_funcao -> snake_case === variáveis e funções
+# EssaEMinhaFuncao -> cammelCase
+# MinhaFuncao -> PascalCase  === classes
